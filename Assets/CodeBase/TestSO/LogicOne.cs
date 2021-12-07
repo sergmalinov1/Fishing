db33425f7c27f77960660bafed4c242541a5978b
sergmalinov <sergmalinov@gmail.com>
2021-12-08 01:30:11 +0200

    test so

---
+++ Assets/CodeBase/TestSO/LogicOne.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace CodeBase.TestSO
+{
+
+    [CreateAssetMenu(fileName = "LogicOne", menuName = "StaticData/LogicOne", order = 0)]
+    public class LogicOne : ScriptableObject
+    {
+        public int TypeID = 1;
+
+        public void Print()
+        {
+            Debug.Log("LogicOne");
+        }
+    }
+}
---
+++ Assets/CodeBase/TestSO/MainSO.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace CodeBase.TestSO
+{
+    [CreateAssetMenu(fileName = "MainSO", menuName = "StaticData/MainSO", order = 0)]
+    public class MainSO : ScriptableObject
+    {
+        public GameObject gameObject;
+        public ScriptableObject so;
+
+
+        public void Test()
+        {
+            LogicOne logic = so as LogicOne;
+            logic.Print();
+        }
+    }
+}
